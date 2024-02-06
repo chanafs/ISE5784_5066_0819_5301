c6f6aa680215cbd4ea09847adab5712ed81d9540
chanafs <chanashutyak@gmail.com>
2024-02-06 14:34:53 +0200

    adding javadoc

--- SoftwareEngineeringProject/src/geometries/Cylinder.java
+++ SoftwareEngineeringProject/src/geometries/Cylinder.java
@@ -1,21 +1,54 @@
 package geometries;
 import primitives.*; 
-/*
- * Cylinder inherits from Tube*/
-public class Cylinder extends Tube{
-	
-	private double height; 
-	public Vector getNormal(Point p) {
-		return null; 
-	}
-	Ray axis; 
-	/*
-	 * constructor*/
-	public Cylinder(double radius, Ray axis, double height, Ray axis2) {
-		super(radius, axis);
-		this.height = height;
-		axis = axis2;
-	}
+import static primitives.Util.alignZero;
+import static primitives.Util.isZero;
 
+public class Cylinder extends Tube{
+	private double height; 
+	/*
+	 * constructor
+	 *@param radius:The radius of the cylinder base
+     *@param axisRay: The axis ray of the cylinder.
+     *@param height: The height of the cylinder.
+	 */
+	public Cylinder(double radius, Ray axisRay, double height) {
+		super(radius, axisRay);
+		this.height = height;
+	}
+	/*
+    *getHeight returns the height of the cylinder
+    */
+   public double getHeight() {
+       return height;
+   }
+   /*
+    *getNormal returns the normal of the cylinder
+    *@param point
+    */
+   @Override
+   public Vector getNormal(Point point) {
+       Point p0 = axis.getHead();
+       Vector v = axis.getDirection();
+       
+       //returns v because it is in the direction of the axis 
+       if (point.equals(p0))
+           return v;
+       
+       //project point-p0 on the ray 
+       Vector u = point.subtract(p0);
+
+       // distance from p0 to p1
+       double t = alignZero(u.dotProduct(v));
+
+       //if the given point is at the base of the cylinder, return direction vector 
+       if (t == 0 || isZero(height - t))
+           return v;
+       
+       //Calculates the other point on the axis facing the given point
+       Point p1= p0.add(v.scale(t));
+       
+       //return the normalized vector 
+       return point.subtract(p1).normalize();
+   }
 		
 }
--- SoftwareEngineeringProject/src/geometries/Geometries.java
+++ SoftwareEngineeringProject/src/geometries/Geometries.java
@@ -1,44 +1,51 @@
-/**
- * 
- */
 package geometries;
 import primitives.Point;
 import primitives.Ray;
-
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * 
- */
 public class Geometries implements Intersectable{
 	private List<Intersectable> intersectables = null;
-
+	/*
+	* constructor 
+	*/
     public Geometries() {
         this.intersectables= new LinkedList<>();
     }
-
+    /*
+	* constructor
+	* @param intersectables: array of Intersectable objects to be added to intersectables list 
+	*/
     public Geometries(Intersectable... intersectables) {
-        this.intersectables = new LinkedList<>();
+        this.intersectables = new LinkedList<>(); 
         add(intersectables);
     }
-
-  public void add(Intersectable... intersectables) {
+    /*
+	* constructor
+	* @param intersectables: array of Intersectable objects to be added to intersectables list 
+	*/
+    public void add(Intersectable... intersectables) {
         for(Intersectable item : intersectables)
         {
             this.intersectables.add(item);
         }
     }
-
+    /*
+	* constructor
+	* @param ray 
+	*/
     @Override
     public List<Point> findIntersections(Ray ray) {
+    	//stores the intersection Points in result 
     	List<Point> result = null;
         for(Intersectable item : this.intersectables){
             List<Point>itemPoints = item.findIntersections(ray);
+            //if there are intersections with the current Intersectable object
             if(itemPoints!= null){
                 if(result == null){
                     result = new LinkedList<>();
                 }
+                //add intersections to result 
                 result.addAll(itemPoints);
             }
         }
