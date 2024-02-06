1ebe7e44ec6df8353efa8667dcbbaaa9fbfc9713
chanafs <chanashutyak@gmail.com>
2024-02-06 15:25:58 +0200

    pushing all changes from chana's eclipse 3:25

--- SoftwareEngineeringProject/src/geometries/Intersectable.java
+++ SoftwareEngineeringProject/src/geometries/Intersectable.java
@@ -1,13 +1,8 @@
 package geometries;
 import primitives.*;
-import java.util.List;
-
-<<<<<<< Upstream, based on origin/main
-/** interface for finding intersection points
-=======
+import java.util.List; 
 /*
  * Intersectable is an interface for finding intersection points
->>>>>>> 41e167c latest changes 6/2/24
 */
 public interface Intersectable {
      List<Point> findIntersections(Ray ray);
